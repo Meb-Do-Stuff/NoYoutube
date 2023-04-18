20528a8e8c593067b69ed095773f74fd7a3dd4d6
MeblIkea <69876322+MeblIkea@users.noreply.github.com>
2023-04-18 11:24:42 +0200

    Chrome thing update

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 function getStored(item, key) {
     let value = item[key];
     if (value === undefined) {
-        browser.storage.sync.set({[key]: true});
+        chrome.storage.sync.set({[key]: true});
         value = true
     }
     return [key, value];
 }
 
-browser.storage.sync.get("homepage", function (item) {
+chrome.storage.sync.get("homepage", function (item) {
     if (getStored(item, "homepage")[1] === true) {
         waitForElm("#contents").then( function () {
             document.getElementById("contents").remove();
             console.log("Videos removed");
         });}
 })
-browser.storage.sync.get("shorts_button", function (item) {
+chrome.storage.sync.get("shorts_button", function (item) {
     if (getStored(item, "shorts_button")[1] === true) {
     waitForElm(".ytd-guide-section-renderer").then( function () {
     for (let element of document.getElementsByTagName("a")) {
@@ -22,14 +22,14 @@ browser.storage.sync.get("shorts_button", function (item) {
     }});}
 })
 
-browser.storage.sync.get("shorts_suggestions", function (item) {
+chrome.storage.sync.get("shorts_suggestions", function (item) {
     if (getStored(item, "shorts_suggestions")[1] === true) {
     waitForElm(".ytd-guide-section-renderer").then( function () {
     for (let element of document.getElementsByTagName("ytd-rich-shelf-renderer")) {
         if (element.getAttribute("is-shorts") !== null) {element.remove();console.log("Shorts suggestions removed")}
     }});}
 })
-browser.storage.sync.get("suggestion_part", function (item) {
+chrome.storage.sync.get("suggestion_part", function (item) {
     if (getStored(item, "suggestion_part")[1] === true) {
     waitForElm(".ytd-watch-next-secondary-results-renderer").then( function () {
     for (let element of document.getElementsByClassName("ytd-watch-next-secondary-results-renderer")) {
@@ -43,7 +43,7 @@ function remove_end_suggestions() {
         element.remove();console.log("Videos suggestions removed");remove_end_suggestions();
     }});
 }
-browser.storage.sync.get("end_video_suggestions", function (item) {
+chrome.storage.sync.get("end_video_suggestions", function (item) {
     if (getStored(item, "end_video_suggestions")[1] === true) {remove_end_suggestions();}
 })
 
--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 function getStored(item, key) {
     let value = item[key];
     if (value === undefined) {
-        browser.storage.sync.set({[key]: true});
+        chrome.storage.sync.set({[key]: true});
         value = true
     }
     return [key, value];
@@ -18,27 +18,27 @@ shorts_button_label = document.getElementById("dshortsbutton");
 end_video_label = document.getElementById("dendvideo");
 video_suggestion_label = document.getElementById("dsuggestionpart");
 
-browser.storage.sync.get("homepage", function (item) {
+chrome.storage.sync.get("homepage", function (item) {
     if (getStored(item, "homepage")[1] !== homepage_label.getElementsByTagName("input")[0].checked) {
         homepage_label.getElementsByTagName("input")[0].click();
     }
 })
-browser.storage.sync.get("shorts_suggestions", function (item) {
+chrome.storage.sync.get("shorts_suggestions", function (item) {
     if (getStored(item, "shorts_suggestions")[1] !== shorts_suggestions_label.getElementsByTagName("input")[0].checked) {
         shorts_suggestions_label.getElementsByTagName("input")[0].click();
     }
 })
-browser.storage.sync.get("shorts_button", function (item) {
+chrome.storage.sync.get("shorts_button", function (item) {
     if (getStored(item, "shorts_button")[1] !== shorts_button_label.getElementsByTagName("input")[0].checked) {
         shorts_button_label.getElementsByTagName("input")[0].click();
     }
 })
-browser.storage.sync.get("end_video_suggestions", function (item) {
+chrome.storage.sync.get("end_video_suggestions", function (item) {
     if (getStored(item, "end_video_suggestions")[1] !== end_video_label.getElementsByTagName("input")[0].checked) {
         end_video_label.getElementsByTagName("input")[0].click();
     }
 })
-browser.storage.sync.get("suggestion_part", function (item) {
+chrome.storage.sync.get("suggestion_part", function (item) {
     if (getStored(item, "suggestion_part")[1] !== video_suggestion_label.getElementsByTagName("input")[0].checked) {
         video_suggestion_label.getElementsByTagName("input")[0].click();
     }
@@ -51,47 +51,47 @@ homepage_label.getElementsByTagName("input")[0].onclick = function () {
             shorts_suggestions_label.getElementsByTagName("input")[0].click();
             shorts_suggestions_label.getElementsByTagName("input")[0].disabled = true;
         }
-        browser.storage.sync.set({"homepage": true});
+        chrome.storage.sync.set({"homepage": true});
     } else {
         homepage_label.getElementsByTagName("span")[0].innerText = "Disabled";
         shorts_suggestions_label.getElementsByTagName("input")[0].click();
         shorts_suggestions_label.getElementsByTagName("input")[0].disabled = false;
-        browser.storage.sync.set({"homepage": false});
+        chrome.storage.sync.set({"homepage": false});
     }
 }
 shorts_suggestions_label.getElementsByTagName("input")[0].onclick = function () {
     if (shorts_suggestions_label.getElementsByTagName("input")[0].checked) {
         shorts_suggestions_label.getElementsByTagName("span")[0].innerText = "Enabled";
-        browser.storage.sync.set({"shorts_suggestions": true});
+        chrome.storage.sync.set({"shorts_suggestions": true});
     } else {
         shorts_suggestions_label.getElementsByTagName("span")[0].innerText = "Disabled";
-        browser.storage.sync.set({"shorts_suggestions": false});
+        chrome.storage.sync.set({"shorts_suggestions": false});
     }
 }
 shorts_button_label.getElementsByTagName("input")[0].onclick = function () {
     if (shorts_button_label.getElementsByTagName("input")[0].checked) {
         shorts_button_label.getElementsByTagName("span")[0].innerText = "Enabled";
-        browser.storage.sync.set({"shorts_button": true});
+        chrome.storage.sync.set({"shorts_button": true});
     } else {
         shorts_button_label.getElementsByTagName("span")[0].innerText = "Disabled";
-        browser.storage.sync.set({"shorts_button": false});
+        chrome.storage.sync.set({"shorts_button": false});
     }
 }
 end_video_label.getElementsByTagName("input")[0].onclick = function () {
     if (end_video_label.getElementsByTagName("input")[0].checked) {
         end_video_label.getElementsByTagName("span")[0].innerText = "Enabled";
-        browser.storage.sync.set({"end_video_suggestions": true});
+        chrome.storage.sync.set({"end_video_suggestions": true});
     } else {
         end_video_label.getElementsByTagName("span")[0].innerText = "Disabled";
-        browser.storage.sync.set({"end_video_suggestions": false});
+        chrome.storage.sync.set({"end_video_suggestions": false});
     }
 }
 video_suggestion_label.getElementsByTagName("input")[0].onclick = function () {
     if (video_suggestion_label.getElementsByTagName("input")[0].checked) {
         video_suggestion_label.getElementsByTagName("span")[0].innerText = "Enabled";
-        browser.storage.sync.set({"suggestion_part": true});
+        chrome.storage.sync.set({"suggestion_part": true});
     } else {
         video_suggestion_label.getElementsByTagName("span")[0].innerText = "Disabled";
-        browser.storage.sync.set({"suggestion_part": false});
+        chrome.storage.sync.set({"suggestion_part": false});
     }
 }
